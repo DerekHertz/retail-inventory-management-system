060735a8137c9e406e37f5af8b78ba768804d06c
Derek Hertzell <97211702+DerekHertz@users.noreply.github.com>
2025-06-23 14:45:29 -0700

    add inventory app (#1)
    * add inventory app

---
+++ d287_project/src/main/java/com/example/demo/repositories/SupplierRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repositories;
+
+import com.example.demo.domain.Supplier;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ *
+ *
+ *
+ */
+public interface SupplierRepository extends CrudRepository<Supplier,Long> {
+}
---
+++ src/main/java/com/example/demo/domain/Supplier.java
@@ -0,0 +1,27 @@
+package com.example.demo.domain;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+/**
+ *
+ *
+ *
+ *
+ */
+@Entity
+@DiscriminatorValue("2")
+public class Supplier extends Part{
+String companyName;
+
+    public Supplier() {
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
